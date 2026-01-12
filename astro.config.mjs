39a25087b4e97bd697e3bdc9b9edc85e0d140f7b
Ian Rodrigues <ian.rodrigues@usf.edu.br>
2026-01-12 16:32:55 -0300

    Configure Astro base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://globalionesia-alt.github.io',
+  base: '/meu-portfolio',
   integrations: [tailwind()],
 });
